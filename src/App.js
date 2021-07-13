b9c7a1b02e25ee0c7b066ad4485017965c0a9896
ushisha <usisha.s@gmail.com>
2021-07-13 22:24:34 +0100

    added gitignore & screenshot

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import React,{ useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
-  const [count,setCount] = useState(0);
-  const [text,setText] = useState([]);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-  const handleSubmit=(e)=>{
-e.preventDefault(); //prevent unnesecery page refresh
-let amount = parseInt(count)
-if(amount <=0) {
-  amount= 1;
-}
-if(amount >8) {
-  amount = 8;
-}
-setText(data.slice(0,amount));
-
-
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault(); //prevent unnesecery page refresh
+    let amount = parseInt(count);
+    if (amount <= 0) {
+      amount = 1;
+    }
+    if (amount > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
     <section className="section-center">
       <h3>Lorem Ipsum Generator</h3>
-      <form className= "lorem-form" onSubmit= {handleSubmit}>
-        <lebel htmlFor='amount'>
-          Paragraphs:
-        </lebel>
-        <input type="number" name = 'amount' id="amount" value={count} onChange={(e)=> setCount(e.target.value)}/>
-        <button type="submit" className="btn">generate</button>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <lebel htmlFor="amount">Paragraphs:</lebel>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
       </form>
-      <article className='lorem-text' >
-        {
-          text.map((item,index)=>{
-return <p key={index}>{item}</p>
-          })
-          
-          }
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
